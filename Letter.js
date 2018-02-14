792b43d8c51fe53e67b8a8dbfb987310099227f7
Nathan <nathanalston@gmail.com>
2018-02-14 16:57:06 -0500

    Worked on Letter.js

--- Letter.js
+++ Letter.js
@@ -1,3 +1,26 @@
 // Letter.js should not require any other files
 // Constructor named Letter
 // Constructor should be able to either display an underlying character or a blank placeholder
+var Letter = function(letter, guessed) {
+  this.letter = letterString;
+  // if the letter is false
+  this.guessed = false;
+
+  // return letter if guess is correct underscore if not
+  this.output = function() {
+    if (this.guessed === true) {
+      return this.letter;
+    } else {
+      return "_";
+    }
+  };
+
+  // Compare letters and guesses
+  this.compareLetter = function(guess) {
+    if (guess === this.letter) {
+      this.guessed = true;
+    }
+  };
+}; //End of letter function
+
+module.exports = letter;
