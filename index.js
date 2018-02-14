698ed8b1c03156ec54a6b33922c3c35be7dd9b93
Nathan <nathanalston@gmail.com>
2018-02-14 18:08:08 -0500

    Worked on index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var inquirer = require("inquirer");
 var clear = require("clear");
 
 // Global Variables
-var currentWord, wordObject, guessesRemaining, lettersRemaining, lettersGuessed, trash;
+var currentWord, wordObject, remainingWords, guessesRemaining, lettersRemaining, lettersGuessed, trash;
 
 //Array of variables
 var wordBank = ["awkward", "bangpipes", "banjo", "bungler", "croquet", "crypt", "dwarves", "fervid", "fishhook", "gazebo",
@@ -24,6 +24,27 @@ wordObject = {};
 guessesRemaining = 10;
 var randomWord = Math.floor(Math.random() * wordBank.length);
 currentWord = wordBank[randomWord];
-wordObject = 
+wordObject = new Word(currentWord);
 
+// Remove the word from the list
+wordBank.splice(randomWord, 1);
+remainingWords = wordBank.length;
+guess();
  }
+
+var guess = function() {
+  clear();
+  showLettersRemaining();
+
+if (lettersRemaining === 0) {
+  if (remainingWords === 0){
+    // new game start
+  } else {
+    // next round
+  } else if (guessesRemaining === 0) {
+    // game over
+  } 
+}
+
+
+}
